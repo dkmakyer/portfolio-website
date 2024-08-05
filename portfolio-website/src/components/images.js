3653af22e54ae2495c8de59827bfea793b355d46
dkmakyer <kwasiakyer2105@gmail.com>
2024-08-05 16:28:26 +0000

    image change
    switched from using font-awesome icons to images

---
+++ portfolio-website/src/components/images.js
@@ -0,0 +1,23 @@
+import html from "../assets/html-5.png";
+import css from "../assets/css-3.png";
+import js from "../assets/js.png";
+import react from "../assets/react.svg";
+import bootstrap from "../assets/bootstrap-5-1.svg";
+import git from "../assets/git.svg";
+import aws from "../assets/aws-2.svg";
+import tailwindcss from "../assets/tailwindcss.svg";
+import svelte from "../assets/svelte-1.svg";
+
+const images = {
+    html,
+    css,
+    js,
+    react,
+    bootstrap,
+    git,
+    aws,
+    tailwindcss,
+    svelte,
+};
+
+export default images;
--- portfolio-website/src/pages/home/Home.jsx
+++ portfolio-website/src/pages/home/Home.jsx
@@ -1,66 +1,63 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import "./home.css";
-import CardText from "react-bootstrap/esm/CardText";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faDownload, faHtml5, faCss3Alt} from '@fortawesome/free-solid-svg-icons';
-library.add(faDownload, faHtml5, faCss3Alt);
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './home.css';
+import images from "../../components/images.js";
+
 
 const Home = () => {
+  const { html, css, js, react, bootstrap, git, aws, tailwindcss, svelte } = images;
+
   return (
-    <div>
-      <div className="home-page">
-        <div className="intro">
-          <Card className="card-container">
-            <Card.Body>
-              <Card.Text>
-                <div className="top">
-                  <span className="greeting">Hello, I am</span>
-                  <span className="name">DAVID KWASI MENSAH AKYER</span>
-                  <span className="job">
-                    Frontend Developer & AWS Certified Cloud Practitioner
-                  </span>
-                </div>
-                <div className="self-intro">
-                  <p>
-                    Hi! I’m David Akyer, a frontend developer who loves
-                    transforming UI designs into visually appealing and
-                    functional websites. I am passionate about crafting
-                    user-friendly interfaces and dedicated to delivering
-                    high-quality work. Currently, I am expanding my skills to
-                    become a full-stack developer. Explore my portfolio to see
-                    my journey and projects!
-                  </p>
-                </div>
-                <p></p>
-              </Card.Text>
-              <div className="card-buttons">
-                <Button className="button first">Hire me</Button>
-                <Button className="button second">Download Resume<FontAwesomeIcon className="font-icon" icon="fa-download" /></Button>
+    <div className="home-page">
+      <div className="intro">
+        <Card className="card-container">
+          <Card.Body>
+            <Card.Text>
+              <div className="top">
+                <span className="greeting">Hello, I am</span>
+                <span className="name">DAVID KWASI MENSAH AKYER</span>
+                <span className="job">
+                  Frontend Developer & AWS Certified Cloud Practitioner
+                </span>
               </div>
-            </Card.Body>
-          </Card>
-        </div>
-        <div className="skills">
-          <Card> 
-            <CardText>
-              <p>
-                <span><FontAwesomeIcon className="font-icon" icon="fa-html5" />HTML</span>
-                <span><FontAwesomeIcon className="font-icon" icon="fa-css3-alt" />CSS</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />Javascript</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />React</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />Tailwind CSS</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />Bootstrap</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />Svelte</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />Sveltekit</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />Git</span>
-                <span><FontAwesomeIcon className="font-icon" icon="coffee" />AWS</span>
-              </p>
-            </CardText>
-          </Card>
-        </div>
+              <div className="self-intro">
+                Hi! I’m David Akyer, a frontend developer who loves
+                transforming UI designs into visually appealing and
+                functional websites. I am passionate about crafting
+                user-friendly interfaces and dedicated to delivering
+                high-quality work. Currently, I am expanding my skills to
+                become a full-stack developer. Explore my portfolio to see
+                my journey and projects!
+              </div>
+            </Card.Text>
+            <div className="card-buttons">
+              <Button className="button first">Hire me</Button>
+              <Button className="button second">
+                Download Resume
+              </Button>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
+      <div className="skills">
+        <Card>
+          <Card.Body>
+            <Card.Text>
+              <div className='images'>
+                <span className='img-block'><img src={html} />HTML</span>
+                <span className='img-block'><img src={css} />CSS</span>
+                <span className='img-block'><img src={js} />JavaScript</span>
+                <span className='img-block'><img src={react} />React</span>
+                <span className='img-block'><img src={aws} />AWS</span>
+                <span className='img-block'><img src={bootstrap} />Bootstrap</span>
+                <span className='img-block'><img className='gitImage' src={git} />Git</span>
+                <span className='img-block'><img src={svelte} />Svelte</span>
+                <span className='img-block'><img src={tailwindcss} />TailwindCSS</span>
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
