1dcc17cd70750cd6b0a6602cd58168d5ecd826d1
dkmakyer <kwasiakyer2105@gmail.com>
2024-09-23 14:01:39 +0000

    updated contactMe.jsx
    added form submission to the component. yet to work on the backend code for my mail to receive the actual email message.

--- portfolio-website/src/pages/contact-me/ContactMe.jsx
+++ portfolio-website/src/pages/contact-me/ContactMe.jsx
@@ -1,34 +1,67 @@
 import React from 'react'
 import "./contact-me.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faLinkedin} from '@fortawesome/free-brands-svg-icons'; 
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const ContactMe = () => {
     const [isActive, setIsActive] = React.useState(false);
-    function openForm(){
+    const [formData, setFormData] = React.useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    function openForm() {
         setIsActive(prev => !prev);
     }
-  return (
-    <>
-        <div className="contact-me-container">
-            <h5>Get in Touch</h5>
-            <h1>Contact me</h1>
-            <div className="mode-of-contact">
-                <button onClick={openForm}><FontAwesomeIcon icon={faEnvelope}/>Send me a mail</button>
-                <button><FontAwesomeIcon icon={faLinkedin}/>LinkedIn</button>
-            </div>
-            {isActive ? (
-                <div className="form-container">
-                    <input type="text" placeholder='Name: '/>
-                    <input type="text" placeholder='Email: '/>
-                    <textarea name="message" id="message-box" placeholder="Message: " cols="30" rows="10"></textarea>
-                    <input type="submit" placeholder='send' />
+
+    function openLinkedIn() {
+        window.open("https://www.linkedin.com/in/david-akyer/", "_blank");
+    }
+
+    function handleChange(e) {
+        const { name, value } = e.target;
+        setFormData(prevFormData => ({
+            ...prevFormData, [name]: value
+        }));
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        alert(`Sending: name: ${formData.name}, email: ${formData.email}, message: ${formData.message}`);
+
+        setFormData({
+            name: "",
+            email: "",
+            message: ""
+        });
+        const submitButton = document.getElementById("submit-button");
+        if (submitButton) {
+            submitButton.style.backgroundColor = null;
+        }
+    }
+
+    return (
+        <>
+            <div className="contact-me-container">
+                <h5>Get in Touch</h5>
+                <h1>Contact me</h1>
+                <div className="mode-of-contact">
+                    <button onClick={openForm}><FontAwesomeIcon icon={faEnvelope} />Send me a mail</button>
+                    <button onClick={openLinkedIn}><FontAwesomeIcon icon={faLinkedin} />LinkedIn</button>
                 </div>
-            ) : null}
-        </div>
-    </>
-  );
+                {isActive ? (
+                    <div className="form-container">
+                        <input name='name' value={formData.name} type="text" placeholder='Name: ' onChange={handleChange} />
+                        <input name='email' value={formData.email} type="text" placeholder='Email: ' onChange={handleChange} />
+                        <textarea name='message' value={formData.message} onChange={handleChange} id="message-box" placeholder="Message: " cols="30" rows="10"></textarea>
+                        <input type="submit" onClick={handleSubmit} id='submit-button' />
+                    </div>
+                ) : null}
+            </div>
+        </>
+    );
 }
 
 export default ContactMe
